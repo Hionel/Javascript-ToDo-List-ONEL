64bcc64defac1817d2c6a7adf542979db9ecb2d5
Hionel <104563991+Hionel@users.noreply.github.com>
2022-10-03 17:32:31 +0300

    Small modifications commit

--- ToDo-List/script.js
+++ ToDo-List/script.js
@@ -1,26 +1,27 @@
 let addTaskButton = document.getElementById("submitNewTask");
 let listOfTasks = document.getElementById("listOfTasks");
+let listOfTasksContainer = document.getElementById("title_and_list_container");
 
 addTaskButton.addEventListener("click", function () {
 	let newTaskTitle = document.getElementById("inputNewTask").value;
-	if (newTaskTitle) {
+	if (newTaskTitle && newTaskTitle.trim().length != 0) {
 		let ListItem = document.createElement("li");
 		ListItem.classList.add("taskListItem");
 
-		let taskTitleText = document.createElement("p");
-		taskTitleText.innerText = newTaskTitle;
+		let taskTitleText = document.createElement("input");
+		taskTitleText.value = newTaskTitle;
+		taskTitleText.readOnly = true;
 		// Edit Button
 		let editTitleButton = document.createElement("button");
 		editTitleButton.innerText = "Edit";
 		editTitleButton.addEventListener("click", () => {
 			if (editTitleButton.innerText === "Edit") {
-				// INPUT WITH READ ONLY INSTEAD OF CONTENTEDITABLE ON PARAGRAPH
-				taskTitleText.setAttribute("contentEditable", "true");
+				taskTitleText.readOnly = false;
 				editTitleButton.innerText = "Save Changes";
 				taskTitleText.focus();
 			} else if (editTitleButton.innerText === "Save Changes") {
-				if (taskTitleText.innerText !== "") {
-					taskTitleText.setAttribute("contentEditable", "false");
+				if (taskTitleText.value.trim().length != 0) {
+					taskTitleText.readOnly = true;
 					editTitleButton.innerText = "Edit";
 				} else {
 					taskTitleText.focus();
@@ -41,9 +42,15 @@ addTaskButton.addEventListener("click", function () {
 				return;
 			}
 		});
-
+		// Completion validation
 		let completedTaskCheckbox = document.createElement("input");
 		completedTaskCheckbox.setAttribute("type", "checkbox");
+		let taskCompletion = false;
+		completedTaskCheckbox.addEventListener("click", () => {
+			if (completedTaskCheckbox) {
+				taskCompletion = !taskCompletion;
+			}
+		});
 
 		// Appending elements inside the list item
 		ListItem.appendChild(completedTaskCheckbox);
@@ -55,4 +62,7 @@ addTaskButton.addEventListener("click", function () {
 	} else {
 		alert("No Task");
 	}
+	listOfTasksContainer.style.transform = "translateY(0%)";
 });
+
+let content = `<li class="taskListItem></li>`;
